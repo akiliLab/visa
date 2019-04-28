ff765c2d60ccd0608545659a150956f7f8f3313b
MAKOSCAFEE <barnabasmakonda@gmail.com>
2019-04-28 11:50:35 +0300

    added go-microframework

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
+	"github.com/joho/godotenv"
+	"github.com/micro/go-grpc"
+	"github.com/micro/go-micro"
+	pb "github.com/ubunifupay/visa/pb"
+	"github.com/ubunifupay/visa/pkg/visa"
 	"io/ioutil"
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
-	"github.com/ubunifupay/visa/pkg/visa"
 )
 
 var client *visa.Client
+
+type server struct{}
+
+func (s *server) GetForexRate(ctx context.Context, transactionID string, request *pb.VisaForexRequest) (*pb.VisaForexReply, error) {
+
+	response, err := client.GetForexChangeRate(transactionID, request)
+
+	//TODO: Add logics to handle after this
+
+	return response, err
+}
 
 func main() {
 
@@ -32,5 +46,15 @@ func main() {
 	cert, _ := tls.LoadX509KeyPair(certPath, keyPath)
 	ca, _ := ioutil.ReadFile(caPath)
 	client, _ = visa.NewClient(userID, password, cert, ca, visa.SANDBOX)
+
+	service := grpc.NewService(
+		micro.Name("greeter"),
+	)
+
+	service.Init()
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
 
 }
